b665c8dfe8cd79a197516423604fea0c74449dd8
Ofentse Ngatane <ngataneofentse@gmail.com>
2023-12-08 17:31:00 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://dynamic-froyo-de370c.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ofentse Ngatane
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Ofentse-Ngatane/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
