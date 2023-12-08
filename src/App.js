13077b32c09c9d1982d65f7d3fe326c9efbfe2af
Ofentse Ngatane <ngataneofentse@gmail.com>
2023-12-08 17:55:19 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://dynamic-froyo-de370c.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ofentse Ngatane
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Ofentse-Ngatane/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://dynamic-froyo-de370c.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ofentse Ngatane
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ofentse-Ngatane/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
